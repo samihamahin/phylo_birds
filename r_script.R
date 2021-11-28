dfc0741d73217e4468192fd37e4ac18eb1c6ed5c
Samiha Mahin <samihamahin@berkeley.edu>
2021-11-28 01:17:02 -0800

    made phy objects in r script and updated trimmed concatenated to make sure it's all the same length

---
+++ r_script.R
@@ -0,0 +1,9 @@
+library(ape)
+
+cytb_ <- read.dna("align_cytb_bird_trimmed.fas", format="fasta")
+coi_ <- read.dna("align_coi_bird_trimmed.fas", format="fasta")
+coi_cytb_ <- read.dna("align_coi_cytb_bird_trimmed.fas", format="fasta")
+
+write.dna(coi_, "coi_bird.phy", format="interleaved", nbcol=-1,colsep="")
+write.dna(cytb_, "cytb_bird.phy", format="interleaved", nbcol=-1,colsep="")
+write.dna(coi_cytb_, "coi_cytb.phy", format="interleaved", nbcol=-1,colsep="")
