1c5996155552428b9e92a5083b9706fe8d6bd1d3
Samiha Mahin <samihamahin@berkeley.edu>
2021-12-01 08:05:00 -0800

    didn't reroot tree but mapped discrete characteristics onto tree

--- r_phytools_script.R
+++ r_phytools_script.R
@@ -1,3 +1,4 @@
+# Plotting Tree
 library(phytools)
 
 coi_tree <- read.newick("coi_bird.phy_phyml_tree.txt")
@@ -8,4 +9,17 @@ plotTree(coi_tree, ftype="i",fsize=0.7, lwd=2)
 plotTree(cytb_tree, ftype="i",fsize=0.7, lwd=2)
 plotTree(coi_cytb_tree, ftype="i",fsize=0.7, lwd=2)
 
+
+#Re rooting Tree
 View(coi_tree$tip.label)
+
+# Mapping Discrete Characters onto Tree
+migratory_data <- read.csv("migratory_type_bird.csv", row.names=1)
+
+head(migratory_data)
+
+fmode<-as.factor(setNames(migratory_data[,1],rownames(migratory_data)))
+
+dotTree(coi_tree,fmode,colors=setNames(c("red","blue","orange"), c("resident","temperate", "neotropical")),ftype="i",fsize=0.5,lwd=1)
+dotTree(cytb_tree,fmode,colors=setNames(c("red","blue","orange"), c("resident","temperate", "neotropical")),ftype="i",fsize=0.5,lwd=1)
+dotTree(coi_cytb_tree,fmode,colors=setNames(c("red","blue","orange"), c("resident","temperate", "neotropical")),ftype="i",fsize=0.5,lwd=1)
